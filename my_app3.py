7e071f99247d4a4875b1f0717b3e305d3673aaef
mwg-dmg <156708696+mwg-dmg@users.noreply.github.com>
2024-10-02 15:47:15 +0530

    Update my_app3.py

--- my_app3.py
+++ my_app3.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 st.title("Main Page")
 st.title(":blue[Select the page from the side pannel]")
-st.image("IMG_0004.png",caption='Frame')
+st.image("frame1.JPG",caption='Frame')
 
 
 
